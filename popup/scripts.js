e050f831d6d12700e3385fbff2e1ce435f5598f2
LianyuPeng <lpeng@chosan.com>
2026-04-23 10:11:39 -0400

    added detailed comments in scripts.js

--- popup/scripts.js
+++ popup/scripts.js
@@ -3,18 +3,23 @@ const mainToggle = document.getElementById("mainToggle");
 const mainRow = document.getElementById("mainRow");
 const mainDesc = document.getElementById("mainDesc");
 
-// Read saved state on open 
+// Read saved state on open and set the toggle
 chrome.storage.sync.get(["enabled"], (result) => {
   setMainToggle(result.enabled === true);
 });
 
 // Main toggle click 
 mainToggle.addEventListener("click", () => {
-  const newEnabled = mainToggle.getAttribute("aria-pressed") !== "true";
 
-  chrome.storage.sync.set({ enabled: newEnabled });
+  // aria is a html attribute for accessibility (it would announce "toggle button, pressed") 
+  // also stores the state of the button (pressed or not) so we dont need "let isEnabled = false;"
+  // getAttribute returns the value of the attribute (pressed or not) as string
+  const newEnabled = mainToggle.getAttribute("aria-pressed") === "false"; // Flip from current aria-pressed
+
+  chrome.storage.sync.set({ enabled: newEnabled }); // saves the new state to Chrome's storage so it survives page refreshes and browser restarts
   setMainToggle(newEnabled);
 
+  // sends the new state to the content.js so it can start or stop scrubbing the page
   sendToActiveTab({ type: "SET_ENABLED", enabled: newEnabled });
 });
 
@@ -29,9 +34,17 @@ function setMainToggle(enabled) {
 let hasReloaded = false;
 
 function sendToActiveTab(msg) {
+
+  // get all active tabs in the current window
+  // async, so tabs only exists inside the callback
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    if (!tabs[0]?.id) return;
+
+    if (!tabs[0]?.id) return; // check if undefined, return if so
+
+    // send message to content.js
     chrome.tabs.sendMessage(tabs[0].id, msg, () => {
+      
+      // callback for after the message is deliever or failed
       // If content.js wasn't injected yet, reload once so it gets injected fresh
       if (chrome.runtime.lastError && msg.enabled && !hasReloaded) {
         hasReloaded = true;
